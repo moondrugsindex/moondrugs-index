8d775e160caaa5a266de66f56a6afa0b2902a4da
giua1nganvisaoanhchithayminhemtrongbongdemdangmoccheovoinyc <97789236+chemchetchagio@users.noreply.github.com>
2026-04-18 03:17:07 +0000

    Update index.tsx
    Rebased

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
+/* rebased image */
 import Head from 'next/head'
+import Image from 'next/image'
 
 import siteConfig from '../../config/site.config'
 import Navbar from '../components/Navbar'
@@ -9,11 +11,24 @@ import SwitchLayout from '../components/SwitchLayout'
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-white dark:bg-gray-900">
+    <div className="relative flex min-h-screen flex-col items-center justify-center bg-white dark:bg-gray-900">
       <Head>
         <title>{siteConfig.title}</title>
       </Head>
-    <main className="flex w-full flex-1 flex-col bg-gray-50 dark:bg-gray-800" style={{ backgroundImage: 'url(https://img.craftpix.net/2022/12/Free-Mountain-Backgrounds-Pixel-Art3-720x480.webp)', backgroundSize: 'cover', backgroundAttachment: 'fixed' }}>
+
+      {/* Rebased background image */}
+      <div className="fixed inset-0 -z-10 h-full w-full">
+        <Image
+          src="https://wq64c-my.sharepoint.com/personal/nhatnam_repo_wq64c_onmicrosoft_com/_layouts/15/download.aspx?SourceUrl=%2Fpersonal%2Fnhatnam%5Frepo%5Fwq64c%5Fonmicrosoft%5Fcom%2FDocuments%2F20240217%5F134724%5F2%2Ejpg"
+          alt="background"
+          fill
+          className="object-cover object-center"
+          priority
+          quality={100}
+        />
+      </div>
+
+      <main className="flex w-full flex-1 flex-col bg-black/40 backdrop-blur-sm">
         <Navbar />
         <div className="mx-auto w-full max-w-5xl py-4 sm:p-4">
           <nav className="mb-4 flex items-center justify-between px-4 sm:px-0 sm:pl-1">
